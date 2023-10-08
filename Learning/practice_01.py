c7f21e58f3f9e663f1d5a6790bd796a59737276d
Ki988 <gohra159@gmail.com>
2023-10-08 14:00:13 +0900

    Add: Learn String data type

--- Learning/practice_01.py
+++ Learning/practice_01.py
@@ -3,3 +3,9 @@
 print(type(5))         # int type
 print(type(3.14))      # float type
 print(type(4 + 5j))    # complex type
+
+# 01.02 String data type
+
+print('Marigold')    # use ' for str type
+print("Shutter")     # use " for str type
+print('A'*4)         # repeat text use *int
