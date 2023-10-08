ffdf0294015b84ccf014b4f9160b94994c4cd88e
Ki988 <gohra159@gmail.com>
2023-10-08 14:06:23 +0900

    Add: Learn Boolean data type
    - Ture or False
    - use 'not ' for reverse response

--- Learning/practice_01.py
+++ Learning/practice_01.py
@@ -9,3 +9,11 @@ print(type(4 + 5j))    # complex type
 print('Marigold')    # use ' for str type
 print("Shutter")     # use " for str type
 print('A'*4)         # repeat text use *int
+
+# 01.03 Boolean data type
+
+print(5 > 10)          # False
+print(5 < 10)          # True
+print(True)            # True
+print(not Ture)        # False
+print(not (5 > 10))    # True
