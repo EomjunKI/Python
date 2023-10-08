31cc48c6b89dca6748c3030d81dbfd99152ef83a
Ki988 <gohra159@gmail.com>
2023-10-08 14:43:26 +0900

    Add: Learn Variable
    - convert data type when use ' + '
    - do not convert data type when use ' , '

--- Learning/practice_01.py
+++ Learning/practice_01.py
@@ -17,3 +17,17 @@ print(5 < 10)          # True
 print(True)            # True
 print(not True)        # False
 print(not (5 > 10))    # True
+
+# 01.04 Variable
+
+print('This song is Attention by Charle Puth')
+print('Charle Puth is 31')
+print('Is Charlie Puth in his 30s? True')
+
+name = 'Charle Puth'
+age  = 31
+generation_30s = age >= 30 and age < 40
+
+print('This song is Attention by ' + name)    # fix string to variable
+print(name + ' is ' + str(age))               # convert int type into str type
+print('Is', name, 'in his 30s?', generation_30s)    # use , instead of +
