86814ba901cc1ec082733b3f0ca18c9dcb6bd183
Ki988 <gohra159@gmail.com>
2023-10-08 15:38:15 +0900

    Add: Learn Operator

---
+++ Learning/practice_02.py
@@ -0,0 +1,19 @@
+# 02.01 Operator
+
+# arithmetic operator
+a = 5
+b = 2
+
+print(a + b)     # additon
+print(a - b)     # subtraction
+print(a * b)     # multiplication
+print(a / b)     # division
+print(a ** b)    # exponentiation
+print(a % b)     # remainder
+print(a // b)    # quotient
+
+# comparison operator
+print(10 > 3)     # over / under
+print(3 >= 10)    # more than / less than
+print(3 == 3)     # equal
+print(3 != 3)     # not equal
