f5f78b0070291e9772304fb01181e273ea24d42b
Ki988 <gohra159@gmail.com>
2023-10-09 11:49:31 +0900

    Add: Learn Slicing
    - [ A : B : C ] mean more than A, under B, at intervals of C

--- Learning/practice_03.py
+++ Learning/practice_03.py
@@ -7,3 +7,11 @@ till were 70
 '''    # can change ''' to """
 
 print(sentence)
+
+# 03.02 Slicing
+
+data = "20231009EdSheeran"
+
+print('year : ' + data[0:4])      # from position 0 to position 3
+print('singer : ' + data[8:])     # from position 8 to the end
+print('singer : ' + data[-9:])    # on the contrary
