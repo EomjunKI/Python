b7fbabce87ab49ee9a0c519b2b7f0f9c7d244549
Ki988 <gohra159@gmail.com>
2023-10-09 13:18:45 +0900

    Add: Learn String methods
    - find : output -1 when value is not exist
    - index : output errorcode when value is not exist

--- Learning/practice_03.py
+++ Learning/practice_03.py
@@ -15,3 +15,22 @@ data = "20231009EdSheeran"
 print('year : ' + data[0:4])      # from position 0 to position 3
 print('singer : ' + data[8:])     # from position 8 to the end
 print('singer : ' + data[-9:])    # on the contrary
+
+# 03.03 String methods
+
+lyrics = 'Sing us a song, you\'r the piano man'
+
+print(lyrics.lower())         # in lowercase
+print(lyrics.upper())         # in capital
+print(lyrics[0].isupper())    # position 0 is capital
+print(len(lyrics))            # length of string, without backslash
+print(lyrics.replace('piano man', 'pianist'))
+
+index = lyrics.index('u')    # 1st position of u
+print(index)
+print(lyrics.find('u'))
+print(lyrics.find('b'))      # string has no b
+print(lyrics.count('u'))     # count all u in string
+
+index = lyrics.index('u', index + 1)    # 2nd position of u
+print(index)
