2db86e290448fbe0b19efd32b69d9acb9ef792fa
Ki988 <gohra159@gmail.com>
2023-10-13 19:12:23 +0900

    Add: Solve question about count passenger program

--- Learning/practice_05.py
+++ Learning/practice_05.py
@@ -55,3 +55,55 @@ for num in range(10):
 playlist = ["STAY", "Flowers", "Attention"]
 playlist_len = [len(i) for i in playlist]
 print(playlist_len)
+
+# 05.06 Quiz
+
+"""
+You are Uber driver.
+Write a program that to find the number of passengers on board.
+However, you must meet the following requirements.
+
+1. Total number of passengers is 50
+2. Time required for each passenger is random between 5 and 50 min
+3. Match time required between 5 and 15 min
+
+Output sample
+[O] 1st passenger (Time required : 7 min)
+[ ] 2nd passenger (Time required : 32 min)
+[O] 3rd passenger (Time required : 8 min)
+ ...
+[ ] 50th passenger (Time required : 20 min)
+
+number of passengers on board : XX
+"""
+
+from random import *
+
+count = 0
+
+for num in range(1, 51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        if num%10 == 1 and num != 11:
+            print('[O] {}st passenger (Time required : {})'.format(num, time))
+            count += 1
+        elif num%10 == 2 and num != 12:
+            print('[O] {}nd passenger (Time required : {})'.format(num, time))
+            count += 1
+        elif num%10 == 3 and num != 13:
+            print('[O] {}rd passenger (Time required : {})'.format(num, time))
+            count += 1
+        else :
+            print('[O] {}th passenger (Time required : {})'.format(num, time))
+            count += 1
+    else:
+        if num%10 == 1 and num != 11:
+            print('[ ] {}st passenger (Time required : {})'.format(num, time))
+        elif num%10 == 2 and num != 12:
+            print('[ ] {}nd passenger (Time required : {})'.format(num, time))
+        elif num%10 == 3 and num != 13:
+            print('[ ] {}rd passenger (Time required : {})'.format(num, time))
+        else :
+            print('[ ] {}th passenger (Time required : {})'.format(num, time))
+            
+print('number of passengers on board : {}'.format(count))
