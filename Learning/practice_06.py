7b633c6873cdbd30d2e1bc5a3bfa8167a54bcf78
Ki988 <gohra159@gmail.com>
2023-10-14 19:27:44 +0900

    Add: Learn default value

--- Learning/practice_06.py
+++ Learning/practice_06.py
@@ -14,3 +14,18 @@ def num_song(num, num_new):
 num = 30    # number of song in playlist already
 num = num_song(num, 20)
 print(num)
+
+# 06.03 Default value
+
+def profile(song, singer):
+    print('Title : {}\t Singer : {}'.format(song, singer))
+
+profile('That\'s Hilarious', 'Charlie Puth')
+profile('Dangerously', 'Charlie Puth')
+
+def profile1(song, singer='Charlie Puth'):    # use repeated inputs
+    print('Title : {}\t Singer : {}'.format(song, singer))
+
+profile1('That\'s Hilarious')
+profile1('Dangerously')
+profile1('Goodbyes', 'Post Malane')
