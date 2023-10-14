53ad394d993619ddd6703835fce5587c397f46d2
Ki988 <gohra159@gmail.com>
2023-10-14 13:47:51 +0900

    Add: Learn input & return
    - reduce repetitive code

--- Learning/practice_06.py
+++ Learning/practice_06.py
@@ -4,3 +4,13 @@ def add_playlist():    # define new funtion
     print('Add a new song.')
 
 add_playlist()
+
+# 06.02 Input & Return
+
+def num_song(num, num_new):
+    print('Number of song in playlist : {}'.format(num + num_new))
+    return num + num_new
+
+num = 30    # number of song in playlist already
+num = num_song(num, 20)
+print(num)
