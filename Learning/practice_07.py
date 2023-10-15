db7da849635cad52fe7e2f3b82d215c4cf7c69be
Ki988 <gohra159@gmail.com>
2023-10-15 16:49:18 +0900

    Add: Learn output format

--- Learning/practice_07.py
+++ Learning/practice_07.py
@@ -14,4 +14,15 @@ for Title, Singer in playlist.items():
     print(Title.ljust(15), Singer.rjust(15), sep=':')
 
 for num in range(1,11):
-    print('Playlist Number : ' + str(num).zfill(3))
+    print('Playlist Number : ' + str(num).zfill(3)) # fill remaining space with zero
+
+# 07.02 Output format
+
+print('{0: >10}'.format(500))     # leave 10 space & output 500 right sorted
+print('{0: >+10}'.format(-500))   # output sign & 500
+print('{0:_<+10}'.format(500))    # leave 10 space & output 500 left sorted
+print('{0:,}'.format(10000000))
+print('{0:+,}'.format(10000000))
+print('{0:^<+15,}'.format(10000000))
+print('{0:f}'.format(5/3))
+print('{0:.2f}'.format(5/3))
