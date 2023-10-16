819e8b0c5c32991fcc9cac0a1be9c977e5096beb
Ki988 <gohra159@gmail.com>
2023-10-16 21:31:03 +0900

    Add: Solve question about real estate transaction

--- Learning/practice_08.py
+++ Learning/practice_08.py
@@ -31,3 +31,38 @@ class Detailed_profile_2(Profile):
     def __init__(self, name, title, genre):
         super().__init__(name, title)   # remove self
         self.genre = genre
+
+# 08.04 Quiz
+
+'''
+Write a program for real estate transaction.
+However, you must meet the following requirements.
+
+Output sample
+3 items for sale in total.
+Manhattan Studio Apartment For Rent $1450/mo
+Brooklyn 1 Bed Apartment For Rent $1850/mo
+New Jersey Town House For Sale $1289000
+'''
+
+class House:
+    def __init__(self, location, house_type, deal_type, price):
+        self.location = location
+        self.house_type =  house_type
+        self.deal_type = deal_type
+        self.price = price
+
+    def show_detail(self):
+        print(f'{self.location:<15}{self.house_type:<20}{self.deal_type:<10}${self.price:<10}')
+
+houses = []
+house_1 = House('Manhattan', 'Studio Apartment', 'For Rent', '1450/mo')
+houses.append(house_1)
+house_2 = House('Brooklyn', '1 Bed Apartment', 'For Rent', '1850/mo')
+houses.append(house_2)
+house_3 = House('New Jersey', 'Town House', 'For Sale', '1289000')
+houses.append(house_3)
+
+print(f'{len(houses)} items for sale in total.')
+for house in houses:
+    house.show_detail()
