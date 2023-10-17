8fdae60e60bd3fb5c711a9e102f10aec29dd53cd
Ki988 <gohra159@gmail.com>
2023-10-17 19:01:11 +0900

    Add: Learn raise
    - make an error on purpose

--- Learning/practice_09.py
+++ Learning/practice_09.py
@@ -6,3 +6,13 @@ try:
     print(playlist[num-1])  # line that error occurred
 except IndexError:
     print(f'There\'s no position {num-1}')  # Output when error occurs
+
+# 09.02 Raise
+
+try:
+    num_list = [0, 1, 2]
+    num = 4
+    if num > 3:
+        raise ValueError
+except ValueError:
+    print('over 3') 
