6f88ae5a33a4c21e61e5c73931c16c3d955d9e2c
Ki988 <gohra159@gmail.com>
2023-10-18 12:08:18 +0900

    Add: Learn module
    - use a file in which user define function

---
+++ Learning/practice_10.py
@@ -0,0 +1,16 @@
+# 10.01 Module
+
+import numpy as np  # use 'np' instead of 'numpy'
+
+matrix_1 = np.array([1,2,3])
+print(matrix_1)
+
+from numpy import * # use all functions of numpy
+
+matrix_2 = array([3,4,5])
+print(matrix_2)
+
+from numpy import array as arr  # use 'arr' instead of 'array'
+
+matrix_3 = arr([4,5,6])
+print(matrix_3)
